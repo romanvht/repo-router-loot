caa2f55b6189bb3485bdcbf617f50f5a10b972ec
romanvht <romanvht@gmail.com>
2026-09-14 22:18:37 +1000

    bump version in plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 
 namespace RouterLoot;
 
-[BepInPlugin(Id, "Router Loot", "0.1.0")]
+[BepInPlugin(Id, "Router Loot", "0.1.3")]
 [BepInDependency("REPOLib", "4.2.0")]
 public sealed class Plugin : BaseUnityPlugin
 {
